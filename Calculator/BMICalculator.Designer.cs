b6c712e5b2fcb5ceb5471e578568167f7817a577
300918514 <300918514@PR-A315-13>
2017-08-04 17:09:30 -0400

    Created Calculator

--- Calculator/BMICalculator.Designer.cs
+++ Calculator/BMICalculator.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(288, 262);
             this.Name = "BMICalculator";
             this.Text = "Calculator";
             this.ResumeLayout(false);
